928a22da46f9f97e6e17b49d60263d7871de4a07
huahua hu <2310929948@qq.com>
2024-03-29 03:31:23 +0000

    hu

---
+++ src/main/java/org/example/project2/Packing.java
@@ -0,0 +1,5 @@
+package org.example.project2;
+
+public interface Packing {
+    public String pack();
+ }
---
+++ src/main/java/org/example/project2/Wrapper.java
@@ -0,0 +1,9 @@
+package org.example.project2;
+
+public class Wrapper implements Packing {
+ 
+    @Override
+    public String pack() {
+       return "Wrapper";
+    }
+ }
